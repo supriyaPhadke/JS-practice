513ad0d8243e21d965e05484d968da5dda582e6c
supriyaPhadke <supriyaphadke30@gmail.com>
2025-04-18 17:48:32 +0530

    Add files via upload

---
+++ freelancer.js
@@ -0,0 +1,12 @@
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  const rateOfOneDay = ratePerHour*8
+    return rateOfOneDay
+}
+
+
